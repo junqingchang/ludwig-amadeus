bb8c11dfc4d69d68865efa7beb1f66b503452edc
junqingchang <jqchang95@gmail.com>
2019-07-29 11:21:02 +0800

    Updated a possible way to retrieve data from MIDI

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import os
 import mido
 import csv
 
+
 maestro_dir = Path('maestro-v2.0.0/')  # maestro-v2.0.0 folder with midi
 output_model_dir = Path('checkpoint/')
 if not os.path.exists(output_model_dir):
@@ -51,17 +52,27 @@ class Maestro(Dataset):
         # Open midi file with mido
         midi_file = mido.MidiFile(self.path / item['midi_name'])
 
-        '''
+        
         # For reading the whole file out, read https://mido.readthedocs.io/en/latest/midi_files.html to find out more
+        msgs = {'tempo':None, 'time_signature':{}, 'music':[]}
         for i, track in enumerate(midi_file.tracks):
-            print('Track {}: {}'.format(i, track.name))
             for msg in track:
-                print(msg)
-        '''
+                if i == 0:
+                    if msg.type == 'set_tempo':
+                        msgs['tempo'] = msg.tempo
+                    if msg.type == 'time_signature':
+                        msgs['time_signature']['numerator'] = msg.numerator
+                        msgs['time_signature']['denominator'] = msg.denominator
+                        msgs['time_signature']['clocks_per_click'] = msg.clocks_per_click
+                        msgs['time_signature']['notated_32nd_notes_per_beat'] = msg.notated_32nd_notes_per_beat
+                else:
+                    msgs['music'].append(msg)
 
-        return midi_file
-'''
-# TEST FUNCTION
-maestro = Maestro(maestro_dir)
-maestro[5]
-'''
+        return msgs
+
+if __name__ == '__main__':
+    
+    # TEST FUNCTION
+    maestro = Maestro(maestro_dir)
+    msg = maestro[15]
+    
