433157365dd24dd3733d220d3d16c6377eecc338
junqingchang <jqchang95@gmail.com>
2019-08-09 00:29:30 +0800

    Fixed model and implemented validation

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Seq2Seq(nn.Module):
 
@@ -11,16 +12,15 @@ class Seq2Seq(nn.Module):
         self.decoder_embedding = nn.Embedding(vocab_size, embedding_dim)
         self.decoder = nn.LSTM(embedding_dim, hidden_dim, num_layers, bidirectional=True)
         
-        self.hidden2notes = nn.Linear(hidden_dim, vocab_size)
+        self.hidden2notes = nn.Linear(hidden_dim*2, vocab_size)
 
-    def forward(self, x):
-        print(x.shape)
-        e_embedding = self.embedding(x)
-        print(e_embedding.shape)
-        _, (ht, ct) = self.encoder(e_embedding)
+    def forward(self, encode, decode):
+        e_embedding = self.embedding(encode)
+        _, (hn, cn) = self.encoder(e_embedding)
 
-        d_embedding = self.decoder_embedding(x[:,-1]).unsqueeze(1)
-        print(d_embedding.shape)
-        decode = self.decoder(d_embedding, (ht, ct))
+        d_embedding = self.decoder_embedding(decode).unsqueeze(1)
+        decode, _ = self.decoder(d_embedding, (hn[:,-1,:].unsqueeze(1), cn[:,-1,:].unsqueeze(1)))
         output = self.hidden2notes(decode)
-        return output
+        tag_scores = F.log_softmax(output, dim=2)
+        tag_scores = tag_scores.transpose(1,2).squeeze(2)
+        return tag_scores
--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import numpy as np
 import random
 import json
 from models import *
+import sys
 
 
 maestro_dir = 'maestro-v2.0.0/'  # maestro-v2.0.0 folder with midi
@@ -57,12 +58,13 @@ class Maestro(Dataset):
                     self.info.append({'composer': composer, 'title': title, 'year': year,
                                       'midi_name': midi_name, 'audio_name': audio_name, 'duration': duration})
 
-        if raw:
+        if raw and train_val_test == 'train':
             self.ntoi = {0: 0}  # Dictionary to map the notes
             self.iton = {0: 0}
 
             self.idx_counter = 1  # Counter for notes mapping
 
+            # Notes finding from midi file
             for i, music in enumerate(self.info):
                 if truncate:
                     if i >= truncate:
@@ -82,7 +84,6 @@ class Maestro(Dataset):
                         self.iton[self.idx_counter] = str(notes)
                         self.idx_counter += 1
                     song_notes[time] = self.ntoi[str(notes)]
-                print(song_notes)
                 np.save(self.processed_data +
                         music['midi_name'][5:-4]+'npy', song_notes)
 
@@ -92,41 +93,74 @@ class Maestro(Dataset):
             with open('iton.txt', 'w', encoding='utf-8') as outfile:
                 json.dump(self.iton, outfile)
 
-        self.dataset = sorted(os.listdir(self.processed_data))
-        with open('ntoi.txt', 'r', encoding='utf-8') as infile:
-            self.ntoi = json.load(infile)
+        elif raw and train_val_test != 'train':
+            with open('ntoi.txt', 'r', encoding='utf-8') as infile:
+                self.ntoi = json.load(infile)
 
-        with open('iton.txt', 'r', encoding='utf-8') as infile:
-            self.iton = json.load(infile)
+            with open('iton.txt', 'r', encoding='utf-8') as infile:
+                self.iton = json.load(infile)
+
+            for i, music in enumerate(self.info):
+                if truncate:
+                    if i >= truncate:
+                        break
+                midi_file = pretty_midi.PrettyMIDI(
+                    self.path + music['midi_name'])
+                piano_midi = midi_file.instruments[0]
+                piano_array = piano_midi.get_piano_roll(fs=30)
+                times = np.unique(np.where(piano_array > 0)[1])
+                index = np.where(piano_array > 0)
+                song_notes = [0] * (times[len(times)-1]+1)
+                for time in times:
+                    index_where = np.where(index[1] == time)
+                    notes = index[0][index_where][-1]
+                    if str(notes) not in self.ntoi:
+                        song_notes[time] = self.ntoi['UNK']
+                    else:
+                        song_notes[time] = self.ntoi[str(notes)]
+                np.save(self.processed_data +
+                        music['midi_name'][5:-4]+'npy', song_notes)
+
+        else:
+            with open('ntoi.txt', 'r', encoding='utf-8') as infile:
+                self.ntoi = json.load(infile)
+
+            with open('iton.txt', 'r', encoding='utf-8') as infile:
+                self.iton = json.load(infile)
+
+        self.dataset = sorted(os.listdir(self.processed_data))
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
         item = np.load(self.processed_data + self.dataset[idx])
-        data, target = self.get_random_seq(item)
-        return torch.tensor(data), torch.tensor(target)
+        encode_data, decode_data, target = self.get_random_seq(item)
+        return torch.tensor(encode_data), torch.tensor(decode_data), torch.tensor(target)
 
     def get_random_seq(self, item):
         index = random.randint(0, len(item))
         if index < self.max_seq:
-            data = item[:index-1]
-            data = np.concatenate(([0] * (self.max_seq-len(data)), data))
+            encode_data = item[:index-2]
+            decode_data = item[index-1]
+            encode_data = np.concatenate(
+                ([0] * (self.max_seq-len(encode_data)-1), encode_data))
         else:
-            data = item[index-1-self.max_seq:index-1]
+            encode_data = item[index-1-self.max_seq:index-2]
+            decode_data = item[index-1]
         target = item[index]
-        return data, target
+        return encode_data, decode_data, target
 
 
 def train(train_loader, model, optimizer, criterion, iterations, device):
     model.train()
     total_loss = 0
     for i in range(iterations):
-        for data, target in train_loader:
-            print('here')
-            data, target = data.to(device), target.to(device)
+        for encode_data, decode_data, target in train_loader:
+            encode_data, decode_data, target = encode_data.to(
+                device), decode_data.to(device), target.to(device)
             optimizer.zero_grad()
-            output = model(data)
+            output = model(encode_data, decode_data)
             loss = criterion(output, target)
             total_loss += loss.item()
             loss.backward()
@@ -134,25 +168,62 @@ def train(train_loader, model, optimizer, criterion, iterations, device):
     return total_loss/len(train_loader)
 
 
+def eval(val_loader, model, criterion, iterations, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad:
+        for i in range(iterations):
+            for encode_data, decode_data, target in val_loader:
+                encode_data, decode_data, target = encode_data.to(
+                    device), decode_data.to(device), target.to(device)
+                output = model(encode_data, decode_data)
+                loss = criterion(output, target)
+                total_loss += loss.item()
+    return total_loss/len(train_loader)
+
+
 if __name__ == '__main__':
     EPOCHS = 200
     LEARNING_RATE = 0.01
     MOMENTUM = 0.9
-    ITERATIONS = 1000
+    ITERATIONS = 1
 
     EMBEDDING_DIM = 128
     HIDDEN_DIM = 128
     N_LAYERS = 1
 
-    maestro = Maestro(maestro_dir, features_dir, raw=True)
+    maestro = Maestro(maestro_dir, features_dir, raw=False)
     train_loader = DataLoader(maestro, batch_size=8, shuffle=True)
-    print(len(maestro.iton))
+
+    validation = Maestro(maestro_dir, features_dir,
+                         train_val_test='validation', raw=True)
+    val_loader = DataLoader(validation, batch_size=8) 
 
     criterion = nn.NLLLoss()
     model = Seq2Seq(len(maestro.iton), EMBEDDING_DIM, HIDDEN_DIM, N_LAYERS)
+    model.to(device)
     optimizer = optim.SGD(model.parameters(),
                           lr=LEARNING_RATE, momentum=MOMENTUM)
+
+    best_loss = sys.maxsize
+    early_stop = 0
+    early_stop_threshold = 5
 
     for epoch in range(1, EPOCHS+1):
         train_loss = train(train_loader, model, optimizer,
                            criterion, ITERATIONS, device)
+        val_loss = eval(val_loader, model, criterion, ITERATIONS, device)
+
+        print('Epoch {}, Train Loss: {}, Validation Loss: {}'.format(epoch, train_loss, val_loss))
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            torch.save(model, output_model_dir+'music-{}.pt'.format(val_loss))
+            early_stop = 0
+        
+        else:
+            early_stop += 1
+            if early_stop >= early_stop_threshold:
+                print('Early Stopping')
+                break
+
